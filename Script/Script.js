21a9d51b3629f546df15c6f1a975b88746eab522
Eliott HUBERT <e.hubert@eleve.leschartreux.net>
2022-05-03 15:36:42 +0200

    ajout navbar

--- Script/Script.js
+++ Script/Script.js
@@ -1,8 +1,8 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {progress_bar()};
 
-function myFunction() {
+function progress_bar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+  document.getElementById("progress-bar").style.width = scrolled + "%";
 } 
