f4d97a6f814c15ad894c39fdd8a5a446a49d0e24
Eliott HUBERT <e.hubert@eleve.leschartreux.net>
2022-07-24 15:10:46 +0200

    Refonte global avec BootStrap

--- Script/Script.js
+++ Script/Script.js
@@ -1,51 +1,33 @@
-window.onscroll = function() {progress_bar()};
-
-function progress_bar() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("progress-bar").style.width = scrolled + "%";
-} 
-
 window.onload = function() {
   Particles.init({
     selector: '.background',
     connectParticles: true,
-    maxParticles: 150,
+    maxParticles: 200,
     color: '#C53602',
     speed: 0.5,
     minDistance:150,
-    sizeVariations: 1
-  });
-}; 
-
-
-ScrollReveal().reveal('.onscroll', { delay: 500 });
-
-const allRonds = document.querySelectorAll('.rond');
-const allBoxes = document.querySelectorAll('.box');
-
-const controller = new ScrollMagick.Controller()
-
-allBoxes.forEach(box => {
-
-    for(i = 0; i < allRonds.length; i++){
-
-        if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
-
-            let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
-
-            let scene = new ScrollMagick.Scene({
-                triggerElement: allRonds[i],
-                reverse: false
-            })
-            .setTween(tween)
-            // .addIndicators()
-            .addTo(controller)
-
-        }
-
+    sizeVariations: 1,
+    responsive: [{
+      breakpoint: 992,
+      options: {
+        maxParticles: 200,
+        color: '#48F2E3',
+        connectParticles: false
+      }
+    }, 
+    {
+      breakpoint: 768,
+      options: {
+        maxParticles: 100,
+        connectParticles: true
+      }
+    }, 
+    {
+      breakpoint: 576,
+      options: {
+        maxParticles: 100// disables particles.js
+      }
     }
-
-})
-
+    ]
+});
+}; 
